7e496f16fe6d69c5bef7d7c0ae3d0814bab8df9c
Jason Chen <jchen146@umd.edu>
2024-09-17 18:17:11 -0400

    updated resume and icon

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import { About } from "@/components/pages/About";
-
-export default function Home() {
-  return (
-    <main className="h-screen w-full bg-[#E0E1DD]">
-      <About />
-    </main>
-  );
-}
